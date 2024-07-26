867b617713c46a74592bf282e0bde1bb5e4fb39c
iniola145 <51451121+iniola145@users.noreply.github.com>
2024-07-26 13:58:04 +0100

    first commit

---
+++ src/Movie.jsx
@@ -0,0 +1,17 @@
+import React, { createContext, useState } from 'react';
+
+
+export const Movie = createContext() // Create a context object
+
+const MyMovie = ({children}) => {
+    const [movie, setMovie] = useState([]); // Create a state variable called movie
+    const [mySearch, setMySearch] = useState(''); // Create a state variable called mySearch
+    const [select, setSelect] = useState(''); // Create a state variable called select
+  return (
+    <Movie.Provider value={{movie, setMovie, mySearch, setMySearch, select, setSelect}}>
+        {children}
+    </Movie.Provider>
+  )
+}
+
+export default Movie
