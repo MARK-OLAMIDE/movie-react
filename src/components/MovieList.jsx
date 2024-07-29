ec47a113f574c8b5a9b40186c9f1bf5b35fe79ab
iniola145 <51451121+iniola145@users.noreply.github.com>
2024-07-29 13:06:54 +0100

    lola

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,8 +1,10 @@
 import React, {useContext, useEffect} from 'react'
 import {Movie} from '../Movie'
 
+let url = "https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=messi&key=AIzaSyBuNK8py5fAhr09mmribVdjeS5GJjeJQjI"
+
 const MovieList = () => {
-  let url = "https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=messi&key=AIzaSyBuNK8py5fAhr09mmribVdjeS5GJjeJQjI"
+
   let {movie, setMovie, mySearch, setMySearch, select, setSelect} = useContext(Movie)
   useEffect(() => {
     let getMyMovie = async () => {
